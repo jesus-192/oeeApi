fd0c984c925f21eb779e0148c0adbee0960ac2db
Joasany Chávez <joasany.chavez@outlook.com>
2023-02-03 18:36:34 -0600

    pruebas

--- oee/aspnet-core/src/oee.Application/Productos/ProductoAppService.cs
+++ oee/aspnet-core/src/oee.Application/Productos/ProductoAppService.cs
@@ -1,10 +1,13 @@
-﻿using oee.Clientes;
+﻿using Npgsql;
+using oee.Clientes;
 using oee.Inventario;
 using oee.Inventarios;
+using Renci.SshNet;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using Volo.Abp.Application.Services;
 using Volo.Abp.Domain.Repositories;
@@ -39,13 +42,49 @@ namespace oee.Productos
 
         public async Task<List<ProductoDto>> GetListAsync()
         {
-            var items = await _repository.GetListAsync();
-            return items
-                .Select(item => new ProductoDto
+           
+            try
+            {
+                var pk = new PrivateKeyFile("C:\\Users\\joasa\\Downloads\\soporteaws.pem");
+                var keyFiles = new[] { pk };
+                var methods = new List<AuthenticationMethod>();
+                methods.Add(new PrivateKeyAuthenticationMethod("ubuntu", keyFiles));
+
+                var con = new ConnectionInfo("3.89.201.178", 22, "ubuntu", methods.ToArray());
+                using (var client = new SshClient(con))
                 {
-                    Id = item.Id,
-                    Nombre = item.Nombre
-                }).ToList();
+                    client.Connect();
+                    ForwardedPortLocal port = new ForwardedPortLocal("127.0.0.1", "127.0.0.1", 60941);
+                    client.AddForwardedPort(port);
+                    port.Start();
+
+                    string connString =
+                        $"Server=localhost;Database=Odoo;Port=60941;" +
+                         "User Id=adminjr;Password=!JRadmin!@;" +
+                         "Timeout = 60;CommandTimeout = 60";
+
+                    using (var conn = new NpgsqlConnection(connString))
+                    {
+                        conn.Open();
+                        NpgsqlCommand command = new NpgsqlCommand("SELECT COUNT(*) FROM account_move", conn);
+                        Int64 count = (Int64)command.ExecuteScalar();
+                        conn.Close();
+                    }
+                }
+
+                //var items = await _repository.GetListAsync();
+                //return items
+                //    .Select(item => new ProductoDto
+                //    {
+                //        Id = item.Id,
+                //        Nombre = item.Nombre
+                //    }).ToList();
+            }
+            catch(Exception ex)
+            {
+
+            }
+            return null;
         }
     }
 }
